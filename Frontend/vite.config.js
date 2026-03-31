d112ef7a1aab8f2b422b7d99aacbd5dd2ecf97ed
BahatiPrince07 <inezaremyprince5@gmail.com>
2026-03-31 09:54:12 +0200

    Frontend routing issues fixed

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,8 +1,32 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'https://reception-management-system.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          icons: ['react-icons'],
+          utils: ['axios', 'socket.io-client', 'react-hot-toast']
+        }
+      }
+    }
+  }
 })
