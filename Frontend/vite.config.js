f414006ea905c0e57d3350fb20c20e6d178958bb
BahatiPrince07 <inezaremyprince5@gmail.com>
2026-04-15 13:18:13 +0200

    Fixing loading issues

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,23 +1,25 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  base: '/',
   server: {
     port: 5173,
-    proxy: {
-      '/api': {
-        target: 'https://reception-management-system.onrender.com',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
+    host: true
   },
   build: {
     outDir: 'dist',
     sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          icons: ['react-icons'],
+          utils: ['axios', 'react-hot-toast', 'socket.io-client']
+        }
+      }
+    }
   }
 })
